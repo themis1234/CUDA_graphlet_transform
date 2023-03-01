74d32db39ea94be8d79d64239a8cc5fe3e30b357
Themis <themistoklisarv2001@gmail.com>
2023-03-01 21:41:20 +0200

    Final working

--- sequential.c
+++ sequential.c
@@ -41,7 +41,6 @@ int* c3(int* indices, int n, int* data){
             int width_j = indices[col+1] - start_j;
             int idx_i = 0;
             int idx_j = 0;
-            //printf("%d %d\n",width_i,width_j);
             while(idx_i<width_i&&idx_j<width_j)//matrix mult
             {
                 if(data[start_i+idx_i]==data[start_j+idx_j]){
@@ -77,27 +76,27 @@ int main(int argc, char const *argv[])
     for(int i = 0; i < num_ind; i++){
         fscanf(ind,"%d\n",&indices[i]);
     }    
-    printf("%d\n",indices[num_ind-1]);
 
 
     int num_data;
     ind = fopen("data.txt","r");
     fscanf(ind,"%d\n",&num_data);
-    printf("%d\n",num_data);
     int* data = malloc(sizeof(int)*num_data);
     for(int i = 0; i < num_data; i++){
         fscanf(ind,"%d\n",&data[i]);
     }    
     int n = num_ind-1;
 
-
+    printf("Finging Nodes' degree");
     int* k = p1(indices,n);
+    printf("Nodes' degree found, Calculating 3-cliques...\n");
     int* m = c3(indices,n,data);
-    int** l = p2_3(k,m,indices,data,n);
+    printf("3-cliques found, Calculating 2-paths and biforks...\n");
 
-    // for(int i = 0; i < n; i ++){
-    //     printf("%d %d %d %d\n",k[i],l[0][i],l[1][i], m[i]);
-    // }
+    int** l = p2_3(k,m,indices,data,n);
+    printf("All Done\n");
+
+
 
     return 0;
 }
